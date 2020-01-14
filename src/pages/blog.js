67bdcedc144a35a06d46e489e25cce83d56485f6
gagangaur <gagan1999gaur@gmail.com>
2020-01-14 21:21:41 +0530

    fetched markdown post data on page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout/layout"
 import { graphql, useStaticQuery, Link } from "gatsby"
 
 const Blog = () => {
+  // fetching the markdown post data
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark {
@@ -17,10 +18,19 @@ const Blog = () => {
       }
     }
   `)
-  console.log(data)
   return (
     <Layout>
-      <h3>This is my blogpage</h3>
+      <h3>This is my blogpage.</h3>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
       <Link to="/">Go back</Link>
     </Layout>
   )
